1929a4c65276cf6170725f79addf0def76fbd58f
prothush <prothushmashreky@gmail.com>
2025-03-01 23:23:01 +0600

    Added script function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 const completeButtons= document.querySelectorAll(".complete-btn");
-const currentDateTime = new Date();
-const formattedDateTime = currentDateTime.toLocaleString();  // Local format
-console.log(formattedDateTime);
 for(let i = 0; i < completeButtons.length; i++){
     const completeBtn= completeButtons[i];
     completeBtn.addEventListener("click", function(event){
@@ -24,7 +21,7 @@ for(let i = 0; i < completeButtons.length; i++){
         const div= document.createElement("div");
         div.innerHTML= `
             <div class="bg-[#F4F7FF] p-4 text-justify rounded-lg space-y-3 logs">
-                <p>You have Completed the ${taskTitle} at 12.30 p.M</p>      
+                <p>You have Completed the ${taskTitle} at ${timeFormat(new Date())}</p>      
             </div>
         `
         taskLog.appendChild(div);
@@ -38,3 +35,73 @@ document.getElementById("clear-logs").addEventListener("click", function(event){
     }
 
 });
+
+const date= document.getElementById("date");
+const p= document.createElement("p");
+p.innerHTML= `
+    ${formatDate(new Date())}
+`
+date.appendChild(p);
+let previousTheme= 0;
+document.getElementById("theme-controller").addEventListener("click", function(event){
+
+    event.preventDefault();
+    const colors =({
+        "regular" : "bg-[#F4F7FF]",
+        "green": "bg-green-200",
+        "red": "bg-red-200",
+        "purple": "bg-purple-200",
+        "orange" : "bg-orange-200",
+        "amber" : "bg-amber-200",
+        "yellow" : "bg-yellow-200",
+        "sky" : "bg-sky-200",
+    })
+    const keys= Object.keys(colors);
+    let randIndex, randKey, randValue;
+    for(let i= 0; i < 100000; i++){
+        randIndex= Math.floor(Math.random()*keys.length);
+        randKey= keys[randIndex];
+        randValue= colors[randKey];
+        if(randValue !== previousTheme){
+            break;
+        }
+    }
+    document.getElementById("body").classList.remove(...Object.values(colors));
+    document.getElementById("body").classList.add(randValue);
+    previousTheme= randValue;
+})
+
+function timeFormat(time){
+    let hours= time.getHours();
+    let minutes= time.getMinutes();
+    let seconds= time.getSeconds();
+
+    let period= hours >= 12 ? "PM" : "AM";
+
+    let hoursConvert= hours % 12;
+    hours= hoursConvert === 0 ? 12 : hoursConvert;
+    hours= hours < 10 ? "0"+hours : hours;
+    minutes= minutes < 10 ? "0"+ minutes : minutes;
+    seconds= seconds < 10 ? "0"+ seconds : seconds;
+
+    return `${hours}: ${minutes}: ${seconds} ${period}`;
+}
+
+function formatDate(date){
+    
+    const week= ["Sun", "Mon", "Tue", "Wes", "Thu", "Fri", "Sat"];
+    const months= ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const day= week[date.getDay()];
+    const month= months[date.getMonth()];
+    let dated= date.getDate();
+    const year= date.getFullYear();
+    if(dated < 10){
+        dated= "0"+dated;
+    } else{
+        dated;
+    }
+
+    return `${day}, <strong>${month} ${dated} ${year}</strong>`;
+}
+
